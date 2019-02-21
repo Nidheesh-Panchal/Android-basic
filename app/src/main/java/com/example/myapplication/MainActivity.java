a42f99918f6249c34c757758d090b31b23ed00fa
Nidheesh Panchal <nilu.1999@yahoo.com>
2019-02-22 01:42:46 +0530

    7 tunes of xylophone

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,39 +14,74 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    SoundPool sounds;
+
+    int C;
+    int D;
+    int E;
+    int F;
+    int G;
+    int A;
+    int B;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button roll;
-        roll=(Button)findViewById(R.id.roll_button);
-        final ImageView ldice=(ImageView) findViewById(R.id.firstdie);
-        final ImageView rdice=(ImageView) findViewById(R.id.seconddie);
-        final int[] dice={
-                R.drawable.dice1,
-                R.drawable.dice2,
-                R.drawable.dice3,
-                R.drawable.dice4,
-                R.drawable.dice5,
-                R.drawable.dice6,
-        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            sounds = new SoundPool.Builder()
+                    .setMaxStreams(NR_OF_SIMULTANEOUS_SOUNDS)
+                    .build();
+        }
+        else
+        {
+            // Deprecated way of creating a SoundPool before Android API 21.
+            sounds = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        }
+        C=sounds.load(getApplicationContext(),R.raw.note1_c,1);
+        D=sounds.load(getApplicationContext(),R.raw.note2_d,1);
+        E=sounds.load(getApplicationContext(),R.raw.note3_e,1);
+        F=sounds.load(getApplicationContext(),R.raw.note4_f,1);
+        G=sounds.load(getApplicationContext(),R.raw.note5_g,1);
+        A=sounds.load(getApplicationContext(),R.raw.note6_a,1);
+        B=sounds.load(getApplicationContext(),R.raw.note7_b,1);
+    }
 
-        roll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Log.d("Android","Hello");
+    public void playC(View v) {
+       sounds.play(C, LEFT_VOLUME, RIGHT_VOLUME, 0, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
-                Random ran = new Random();
-                int num=ran.nextInt(6);
-                //Log.d("Android",""+num);
-                ldice.setImageResource(dice[num]);
-                num=ran.nextInt(6);
-                rdice.setImageResource(dice[num]);
-            }
-        });
+    public void playD(View v) {
+        sounds.play(D, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
+    public void playE(View v) {
+        sounds.play(E, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
+    public void playF(View v) {
+        sounds.play(F, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playG(View v) {
+        sounds.play(G, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playA(View v) {
+        sounds.play(A, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playB(View v) {
+        sounds.play(B, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
     }
 }
 
